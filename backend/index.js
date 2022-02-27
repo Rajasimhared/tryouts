09c01d1b45c15197eed660effa37a53a8d0bd429
Rajasimha Reddy <rajasimha5599@gmail.com>
2022-02-27 10:13:52 +0530

    setup login and signup endpoints

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,30 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+var bodyParser = require('body-parser')
+const {
+  notFound,
+  errorHandler
+} = require("./middleware/errorMiddleware");
 
 const app = express();
 app.use(cors());
+// app.use(bodyParser.json())
+app.use(express.json())
+
 dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running");
+// });
 
-app.get("/chats", (req, res) => {
-  res.send({
-    test: "app"
-  });
-});
+app.use('/api/user', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log("server listening on port 4000"));
